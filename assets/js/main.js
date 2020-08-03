8fdaf9cf0b4a33621ab612a2704bc07ef4aac0b6
Amie Johnstone <amiejohnstone18@gmail.com>
2020-08-03 15:21:45 +0000

    Removed the brackets from the cocktail names. Added in the testing and mock-up files to the directory. Spell checked work and beautified code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,37 @@
+var gData = [];
 
- var gData = [];
-
- function setData(data) {
-     console.log(data);
-    gData = data
- }
- function myFunction(id) {
-    let drink = gData.filter(cocktail => {
-        return cocktail.id === id;
-    })
-console.log(drink);
-   document.getElementById("long-island-iced-tea").innerHTML = "<h1>" + drink[0].name + "</h1>"
-    + "<p>" + drink[0].description + "</p>" ;
-document.getElementById("image_thumb_url").innerHTML = `<img src="${drink[0].image_thumb_url}" />` ; //separated to allow the image to display next to the description
+function setData(data) {
+  console.log(data);
+  gData = data;
+}
+function myFunction(id) {
+  let drink = gData.filter((cocktail) => {
+    return cocktail.id === id;
+  });
+  console.log(drink);
+  document.getElementById("long-island-iced-tea").innerHTML =
+    "<h1>" + drink[0].name + "</h1>" + "<p>" + drink[0].description + "</p>";
+  document.getElementById(
+    "image_thumb_url"
+  ).innerHTML = `<img src="${drink[0].image_thumb_url}" />`; //separated to allow the image to display next to the description
 }
 
-
- let headers = {
-    'Authorization': 'Token token=QtxcH4qt0pxEdy3loSTzVQtt'
- }
- let response = fetch("https://api-cocktails.herokuapp.com/api/v1/cocktails", {headers:headers})
- .then(data => data.json()).then(data => {
-    setData(data)
-    console.log(data)
-    let element = document.querySelector('#data');
+let headers = {
+  Authorization: "Token token=QtxcH4qt0pxEdy3loSTzVQtt",
+};
+let response = fetch("https://api-cocktails.herokuapp.com/api/v1/cocktails", {
+  headers: headers,
+})
+  .then((data) => data.json())
+  .then((data) => {
+    setData(data);
+    console.log(data);
+    let element = document.querySelector("#data");
     let buttonArray = [];
-    data.forEach(item => {
-        buttonArray.push(`<button onClick=myFunction(${item.id})>${item.name})</button>`);
-        element.innerHTML = buttonArray.join('');
-
-    })
- })
-
-
- // + "<br />" + `<img src="${drink[0].image_thumb_url}" />`
+    data.forEach((item) => {
+      buttonArray.push(
+        `<button onClick=myFunction(${item.id})>${item.name}</button>`
+      );
+      element.innerHTML = buttonArray.join("");
+    });
+  });
--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,20 @@
 // Followed code from Code Institutes EmailJS lesson 08/07/2020
 
 function sendMail(contactForm) {
-    emailjs.send("gmail", "happy_hour", {
-        "from_name": contactForm.name.value, 
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
+  emailjs
+    .send("gmail", "happy_hour", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      message: contactForm.message.value,
     })
     .then(
-        function(response) {
+      function (response) {
         console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
+      },
+      function (error) {
+        console.log("FAILED", error);
+      }
+    );
 
-        return false;
+  return false;
 }
